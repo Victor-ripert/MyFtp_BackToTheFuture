b310f95f3c685f5b78015ae1669e571ec9b0086b
Victor Ripert <victor.ripert@epitech.eu>
2026-07-07 12:30:16 +0200

    feat: added CommandPwd

---
+++ include/commands/CommandPwd.hpp
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2026
+** myftp
+** File description:
+** CommandPwd.hpp
+*/
+#ifndef COMMANDPWD_HPP
+#define COMMANDPWD_HPP
+
+#include "ACommand.hpp"
+
+class CommandPwd : public ACommand {
+  public:
+    CommandPwd() = default;
+    ~CommandPwd() override = default;
+
+    void execute(const std::string& args, ClientSession client) override {
+      if (!client.loggedIn) {
+        client.responseBuffer = "530 Not logged in.\n";
+        return;
+      }
+      client.responseBuffer = "257 " + client.cwd + " created.\n";
+    }
+};
+
+#endif /* COMMANDPWD_HPP */
